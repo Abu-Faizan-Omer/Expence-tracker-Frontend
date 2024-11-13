3b323c4394a277f5948b33d4bc6a96a57aa14f3a
Abu-Faizan-Omer <abufaizan1997@gmail.com>
2024-11-14 00:23:41 +0530

    login page

--- login/login.js
+++ login/login.js
@@ -1,16 +1,17 @@
-
-
-async function login(event){
-    event.preventDefault()
-    try{
-        const loginDetails={
-            email:event.target.email.value,
-            password:event.target.password.value
+async function login(event) {
+    event.preventDefault();
+    try {
+        const loginDetails = {
+            email: event.target.email.value,
+            password: event.target.password.value
+        };
+        const response = await axios.post(`http://localhost:3000/users/login`, loginDetails);
+        
+        if (response.status === 200) {
+            alert(response.data.message);
         }
-        await axios.post(`http://localhost:3000/users/login`,loginDetails)
-        if(response.status === 201)
-            alert(response.data.message)
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+        console.log(err);
+        document.body.innerHTML += `<div style="color:red;">${err.response ? err.response.data.message : 'Login failed'}</div>`;
     }
 }
